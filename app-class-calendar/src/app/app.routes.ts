69b894dd10a7750a5f9c7862be739bfd9dcfe649
kroshaResheniye <116317977+Vitor-Gabriel-KR@users.noreply.github.com>
2023-04-23 05:37:34 -0300

    Add files via upload

---
+++ app-class-calendar/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'perfil',
+    loadComponent: () => import('./perfil/perfil.page').then( m => m.PerfilPage)
+  },
+  
+  {
+    path: 'placeholder',
+    loadComponent: () => import('./placeholder/placeholder.page').then((m) => m.PlaceholderPage),
+  },
+  {
+    path: 'materias',
+    loadComponent: () => import('./materias/materias.page').then( m => m.MateriasPage)
+  },
+
+];
