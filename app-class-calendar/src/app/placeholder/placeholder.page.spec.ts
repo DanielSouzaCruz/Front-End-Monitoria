67063dbc9c04e90eb49a3904dc699511561345bd
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-04-24 16:50:58 -0300

    Retornado uma classe que deveria estar intacta nicolau

--- app-class-calendar/src/app/placeholder/placeholder.page.spec.ts
+++ app-class-calendar/src/app/placeholder/placeholder.page.spec.ts
@@ -1,15 +1,15 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlaceholderPage } from './placeholder.page';
 
 describe('PlaceholderPage', () => {
   let component: PlaceholderPage;
   let fixture: ComponentFixture<PlaceholderPage>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(PlaceholderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
