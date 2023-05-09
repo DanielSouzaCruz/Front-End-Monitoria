192fb6f5af848a36db3a701e2fca1ddfa58117e2
kroshaResheniye <kroshaResheniye@gmail.com>
2023-05-09 03:22:52 -0300

    resolvendo conflitos css

--- app-class-calendar/src/app/home/home.page.ts
+++ app-class-calendar/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonicModule, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() {}
-
+  goPerfil(){
+    this.navCtrl.navigateForward('perfil')
+  }
 }
