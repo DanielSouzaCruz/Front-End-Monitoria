3665e79389e549d2536bbb88f4c0c023d68d42d4
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-05-19 13:46:22 -0300

    basico service

--- app-class-calendar/src/app/cadastrobasico/cadastrobasico.page.ts
+++ app-class-calendar/src/app/cadastrobasico/cadastrobasico.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-cadastrobasico',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrobasicoPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService: ApiService) {
+    this.createData();
+   }
+  
   ngOnInit() {
   }
 
