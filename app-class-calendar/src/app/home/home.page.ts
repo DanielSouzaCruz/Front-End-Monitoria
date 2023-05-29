e689ca546efdac345dab0e681d74e33c8f04a097
Vitor-Gabriel-KR <kroshaResheniye@gmail.com>
2023-05-29 15:02:37 -0300

    commit do home card

--- app-class-calendar/src/app/home/home.page.ts
+++ app-class-calendar/src/app/home/home.page.ts
@@ -7,9 +7,25 @@ import { IonicModule, NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  showAgendamento: boolean = true;
+  isMenuOpen: number = 0;
+
   constructor(private navCtrl: NavController) {}
 
   goPerfil(){
     this.navCtrl.navigateForward('perfil')
+    
+  }
+  toggleAgendamento() {
+    this.showAgendamento = !this.showAgendamento;
+  }
+
+  toggleMenu(coluna: number) {
+    if (this.isMenuOpen === coluna) {
+      this.isMenuOpen = 0;
+    } else {
+      this.isMenuOpen = coluna;
+    }
   }
 }
+
