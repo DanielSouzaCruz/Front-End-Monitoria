4578f1a18d4cbe24f78321d2a9c81efb58b0fbcd
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-06-18 18:09:32 -0300

    corrigido

--- app-class-calendar/src/app/professor/professor.page.ts
+++ app-class-calendar/src/app/professor/professor.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, NavController } from '@ionic/angular';
+import { AlertController, IonicModule, NavController } from '@ionic/angular';
 import { BuscaDadosService } from '../api/busca-dados.service';
 import { DeletarService } from '../api/deletar.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-professor',
@@ -16,20 +17,32 @@ export class ProfessorPage implements OnInit {
 
   itens : any = []
   public usuario:any
-  userType: any = 'professor'
+  userGroup: any = 'professor'
   public button: any
-  constructor(private NavCtrl:NavController, private service: BuscaDadosService, private excluirProfessor: DeletarService ) { }
+  public userType: any
+
+  constructor(private NavCtrl:NavController, private alertController: AlertController, private service: BuscaDadosService, private excluirProfessor: DeletarService, private route:ActivatedRoute ) { }
  
   public irProfessorDetalhe(professor: any){
     this.NavCtrl.navigateForward('professor-detalhe',{
-      queryParams: { professor: professor }
+      queryParams: { professor: professor, usuario: this.usuario, userType: this.userType }
     })
   }
 
+  async exibirAlerta (mensagem: string){
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: mensagem,
+      buttons: ['OK']
+    });
   
+    await alert.present();
+  }
 
-  public deletarProfessor(professor: any){
-    this.excluirProfessor.deleteUsuarios(this.userType,professor.idProfessor).then((professor)=>{
+
+  public async deletarProfessor(professor: any){
+    await this.exibirAlerta(professor.nome +' excluido, por favor, atualize a pagina');
+    this.excluirProfessor.deleteUsuarios(this.userGroup,professor.idProfessor).then((professor)=>{
 
       console.log("delete")
       this.getAllDados();
@@ -37,14 +50,37 @@ export class ProfessorPage implements OnInit {
   }
 
   public getAllDados () {
-    this.service.getAllDados (this.userType + 'es').then(dados=>{
+    this.service.getAllDados (this.userGroup + 'es').then(dados=>{
       this.itens = dados;
       console.log(this.itens)
     })
 
 }
 
+  public irDisciplina(professor: any){
+    this.NavCtrl.navigateForward('disciplina',{
+      queryParams: { professor: professor, usuario: this.usuario, userType: this.userType }
+    })
+  }
+
 ngOnInit() {
   this.getAllDados()
+  this.route.queryParams.subscribe(params => {
+    this.usuario = params['usuario'];
+    this.userType = params['userType']
+    this.button = params ['button']
+  });
 }
+goHome(){
+  this.NavCtrl.navigateForward('home', {
+    queryParams:  { usuario: this.usuario,
+                  userType: this.userType }
+  });
 }
+
+goPerfil(){
+  this.NavCtrl.navigateForward('perfil', {
+    queryParams: { usuario: this.usuario,
+                   userType: this.userType }
+  });
+}}
